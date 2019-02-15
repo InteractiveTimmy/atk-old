e74cdbcb6286c8240c0e2ee41d198db509d83c95
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-02-14 20:37:59 -0500

    added resolver settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,23 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-        "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"]
+  "extends": "airbnb",
+  "rules": {
+    "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"]
+  },
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "paths": [
+          "src"
+        ],
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
     }
+  }
 };
