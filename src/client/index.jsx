8cdebaeab28405598eb0241a541593ef5b6a74b2
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-02-16 18:06:57 -0500

    updated index to properly connect to store

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,24 +4,14 @@ import ReactDOM from 'react-dom';
 
 // redux
 import { Provider } from 'react-redux';
-// import store from './redux/store';
+import store from './redux/store';
 
 // site components
 import { Desktop } from './sites';
 
-// eslint-disable-next-line no-console
-/*
 ReactDOM.render(
   <Provider store={store}>
     <Desktop />
   </Provider>,
-  document.getElementById('app'),
-);
-*/
-
-ReactDOM.render(
-  <React.Fragment>
-    <Desktop />
-  </React.Fragment>,
   document.getElementById('app'),
 );
